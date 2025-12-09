27590b10a5567b6d2e54a816fe15e6c325d9e0ca
MattW-12106 <12106@holbertonstudents.com>
2025-12-09 10:08:12 +0000

    counts the number of leavews in a tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a tree
+ * @tree: pointer to tree
+ *
+ * Return: number of leaves plus 1 on sucess, 0 and 1 for fail
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_trees_leaves(tree->right));
+}

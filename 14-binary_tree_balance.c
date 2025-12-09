3cdbdc36c09b3a72ead7d0e09ba602c4e81fe62c
MattW-12106 <12106@holbertonstudents.com>
2025-12-09 10:50:06 +0000

    changed height function to if statements

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_h, right_h;
+        size_t left_h = 0, right_h = 0;
 
         if (tree == NULL)
                 return (0);
@@ -16,8 +16,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
         if (tree->left == NULL && tree->right == NULL)
                 return (0);
 
-        left_h = binary_tree_height(tree->left) + 1;
-        right_h = binary_tree_height(tree->right) + 1;
+	if (tree->left)
+        	left_h = binary_tree_height(tree->left) + 1;
+
+	if (tree->right)
+        	right_h = binary_tree_height(tree->right) + 1;
 
         return (left_h > right_h ? left_h : right_h);
 }

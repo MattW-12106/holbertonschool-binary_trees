ac5ce6dd09300bac9fd81cb1edf0eb7633dfea35
MattW-12106 <12106@holbertonstudents.com>
2025-12-09 10:56:37 +0000

    changed height function again

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,21 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_h = 0, right_h = 0;
+	size_t left = 0, right = 0;
 
-        if (tree == NULL)
-                return (0);
-
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
 	if (tree->left)
-        	left_h = binary_tree_height(tree->left) + 1;
+        	left = binary_tree_height(tree->left) + 1;
 
 	if (tree->right)
-        	right_h = binary_tree_height(tree->right) + 1;
+		right = binary_tree_height(tree->right) + 1;
 
-        return (left_h > right_h ? left_h : right_h);
+	return (left > right ? left : right);
 }
 
 /**

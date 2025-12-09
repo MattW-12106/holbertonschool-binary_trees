d93a1ec216747f9cff59029c15e9641bca12e296
MattW-12106 <12106@holbertonstudents.com>
2025-12-09 04:04:38 +0000

    calculates the height of tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
 
-	return (left_h > right_h ? left_h : right_h) + 1;
+	return ((left_h > right_h ? left_h : right_h) + 1);
 }

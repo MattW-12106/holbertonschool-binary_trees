9eaccfa8c93d3f00d6ab09eace12f5bfb8b57efc
MattW-12106 <12106@holbertonstudents.com>
2025-12-09 03:37:46 +0000

    post order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverses tree in post order
+ * @tree: pointer to tree
+ * @func: pointer to function
+ * Return: if tree or func equals NULL
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
